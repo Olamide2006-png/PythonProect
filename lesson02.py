b3d2fd87f33065473e689c7fef0176c1cb5fdafb
Peters Johnson <petersolamide2006@gmail.com>
2025-07-30 20:19:08 +0100

    Add files via upload

---
+++ lesson02.py
@@ -0,0 +1,27 @@
+
+#Numeric types
+num = 5.0
+Num = 55
+print(num,'is of type', type(num))
+print(Num, 'is of type',type(Num))
+
+#List
+languages = ['python','laravel','javascript']
+print(type(languages))
+print(languages[1])
+print(languages[2])
+print(languages[0])
+languages[1] = 'java'
+print(languages)
+
+#tuple
+products =('phone','laptop','tablet')
+print(type(products))
+print(products[0])
+print(products[1])
+print(products[2])
+
+#set
+my_set = {'apple', 'banana', 'cherry'}
+print(type(my_set))
+
---
+++ lesson1.py
@@ -0,0 +1,12 @@
+site_name = 'johnson peters project'
+a,b,c =2,3,5
+y =3.4
+z = True
+print(site_name)
+print(a)
+print(b)
+print(c)
+
+print(type(site_name))
+print(y)
+print(z)
